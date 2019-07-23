84485d636e030458ec026c930b8edc1a390cc78c
Ashish Sahay <Davidashish1996@gmail.com>
2019-07-23 22:47:25 +0530

    morgan_middleware

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const http = require('http');
 
+const morgan = require('morgan');
+
 const hostname = 'localhost';
 
 const port  = 3000;
 
 const app = express();
 
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+ '/public'))
 
 app.use((req,res,next)=>
 {
