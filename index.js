faedaf2bc97d27254a9cedffa2224113b8a14f52
Ashish Sahay <Davidashish1996@gmail.com>
2019-07-23 17:25:13 +0530

    Second Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const http = require('http');
+
+const hostname = 'localhost';
+
+const port  = 3000;
+
+const app = express();
+
+
+app.use((req,res,next)=>
+{
+  res.statusCode = 200;
+  res.setHeader('Content-Type','text/html');
+  res.end('<html> <body> <h1> This is an express server </h1> </body> </html>');
+});
+
+app.listen(port);
